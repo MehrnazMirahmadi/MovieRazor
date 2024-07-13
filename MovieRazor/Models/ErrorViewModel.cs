fade1784f019852895f808bf6da78abefd333ce9
MehrnazMirahmadi <125347723+MehrnazMirahmadi@users.noreply.github.com>
2024-07-13 10:56:40 +0330

    Add project files.

---
+++ MovieRazor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieRazor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
